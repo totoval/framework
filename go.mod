42925fa53635b33c809f5054e23589c1b97f7316
Toby Yan <me@tobyan.com>
2019-02-18 19:21:28 +0800

    add base_model Create func
    Signed-off-by: Toby Yan <me@tobyan.com>

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ require (
 	github.com/fatih/color v1.7.0
 	github.com/gin-contrib/sse v0.0.0-20190125020943-a7658810eb74 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/go-playground/locales v0.12.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.1 // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/jinzhu/gorm v1.9.2
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/leodido/go-urn v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/spf13/viper v1.3.1 // indirect
@@ -16,4 +19,5 @@ require (
 	github.com/urfave/cli v1.20.0 // indirect
 	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/go-playground/validator.v9 v9.27.0 // indirect
 )
--- model/base_model.go
+++ model/base_model.go
@@ -1,16 +1,139 @@
 package model
 
-type BaseModel struct{}
+import (
+	"errors"
+	"gopkg.in/go-playground/validator.v9"
+	"reflect"
+)
+
+type BaseModel struct {}
 
 type Modeller interface {
+	Default() interface{}
 	GetObjArr()         //@todo     public function getObjArr(?array $filter_arr = [], ?array $sort_arr = null, ?int $limit = null, bool $with_trashed = false): Collection;
 	GetObjArrPaginate() //@todo     public function getObjArrPaginate(int $per_page, ?array $filter_arr = [], ?array $sort_arr = null, bool $with_trashed = false): LengthAwarePaginator;
 }
+func structToMap(data interface{}) (result map[string]interface{}) {
+	result = map[string]interface{}{}
 
-func (m *BaseModel) AddObj() { // public function addObj(array $data_arr)
+	dataType := reflect.TypeOf(data)
+	dataValue := reflect.ValueOf(data)
 
+	for i := 0; i < dataType.NumField(); i++ {
+		dataField := dataType.Field(i)
+		dataFieldValue := dataValue.Field(i)
+
+		switch dataFieldValue.Kind() {
+		case reflect.Bool:
+			result[dataField.Name] = dataFieldValue.Bool()
+			break
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			result[dataField.Name] = dataFieldValue.Int()
+			break
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			result[dataField.Name] = dataFieldValue.Uint()
+			break
+		case reflect.String:
+			result[dataField.Name] = dataFieldValue.String()
+			break
+		case reflect.Slice: //TODO...
+		case reflect.Map: //TODO...
+		case reflect.Struct: //TODO...
+		default:
+			panic("struct value cannot be slice, map or struct")
+		}
+	}
+
+	return
 }
-func (m *BaseModel) SaveObj() { //     public function saveObj(array $data_arr, int $id = null, bool $with_trashed = false)
+
+func FillDefault(data map[string]interface{}, defaultData interface{}) (interface{}, error) {
+	defaultDataType := reflect.TypeOf(defaultData)
+	defaultDataValue := reflect.ValueOf(defaultData)
+
+	newDataType := reflect.TypeOf(defaultData)
+	newDataValue := reflect.New(reflect.TypeOf(defaultData)).Elem()
+	for i := 0; i < newDataType.NumField(); i++ {
+		if !newDataValue.Field(i).IsValid() || !newDataValue.Field(i).CanSet() {
+			return nil, errors.New("model value cannot be filled")
+		}
+
+		// fill data
+		isFilled := false
+		for key, value := range data {
+			if newDataType.Field(i).Name == key {
+				newDataValue.Field(i).Set(reflect.ValueOf(value))
+				isFilled = true
+				break
+			}
+		}
+		if isFilled {
+			continue
+		}
+
+		// fill default
+		for j := 0; j < defaultDataType.NumField(); j++ {
+			if newDataType.Field(i).Name == defaultDataType.Field(j).Name {
+				newDataValue.Field(i).Set(defaultDataValue.Field(j))
+				break
+			}
+		}
+
+	}
+
+	return newDataValue.Addr().Interface(), nill
+}
+
+func Create(data interface{}, result interface{}) error {
+	dataMap := structToMap(data)
+
+	defaultData := result.(Modeller)
+	inputData, err := FillDefault(dataMap, defaultData.Default())
+	if err != nil {
+		return err
+	}
+
+	if err := validator.New().Struct(inputData); err != nil {
+		//// this check is only needed when your code could produce
+		//// an invalid value for validation such as interface with nil
+		//// value most including myself do not usually have code like this.
+		//if _, ok := err.(*validator.InvalidValidationError); ok {
+		//	fmt.Println(err)
+		//	return
+		//}
+		//
+		//for _, err := range err.(validator.ValidationErrors) {
+		//
+		//	fmt.Println(err.Namespace())
+		//	fmt.Println(err.Field())
+		//	fmt.Println(err.StructNamespace()) // can differ when a custom TagNameFunc is registered or
+		//	fmt.Println(err.StructField())     // by passing alt name to ReportError like below
+		//	fmt.Println(err.Tag())
+		//	fmt.Println(err.ActualTag())
+		//	fmt.Println(err.Kind())
+		//	fmt.Println(err.Type())
+		//	fmt.Println(err.Value())
+		//	fmt.Println(err.Param())
+		//	fmt.Println()
+		//}
+		//
+		//// from here you can create your own error messages in whatever language you wish
+		return err
+	}
+
+	if err := db.Create(inputData).Error; err != nil {
+		return err
+	}
+
+	result = inputData
+	return nil
+}
+
+//func Save(data interface{}, result interface{}) error {
+//
+//}
+
+func (m *BaseModel) Save() { //     public function saveObj(array $data_arr, int $id = null, bool $with_trashed = false)
 
 }
 
