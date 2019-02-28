01ac983cce686528b4e4b99966cddbf811727d9e
Toby Yan <me@tobyan.com>
2019-02-28 15:34:57 +0800

    modify modeller
    Signed-off-by: Toby Yan <me@tobyan.com>

--- model/model.go
+++ model/model.go
@@ -3,8 +3,6 @@ package model
 
 type Modeller interface {
 	Default() interface{}
-	ObjArr(filterArr [][]interface{}, sortArr []Sort, limit int, withTrashed bool) []interface{} //@todo     public function getObjArr(?array $filter_arr = [], ?array $sort_arr = null, ?int $limit = null, bool $with_trashed = false): Collection;
-	ObjArrPaginate(filterArr [][]interface{}, sortArr []Sort, limit int, withTrashed bool)       //@todo     public function getObjArrPaginate(int $per_page, ?array $filter_arr = [], ?array $sort_arr = null, bool $with_trashed = false): LengthAwarePaginator;
 }
 
 
