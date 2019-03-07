1a67f6619374da0a316dbc96aebe20a49ecc00f9
Toby Yan <me@tobyan.com>
2019-03-07 10:12:07 +0800

    add locale middleware
    Signed-off-by: Toby Yan <me@tobyan.com>

---
+++ http/middleware/locale.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/totoval/framework/config"
+	"github.com/totoval/framework/resources/lang"
+)
+
+func Locale() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		locale := c.Request.Header.Get("locale")
+		if locale == "" {
+			locale = c.DefaultQuery("locale", config.GetString("app.locale"))
+		}
+
+		lang.SetLocale(c, locale)
+
+		c.Next()
+	}
+}
