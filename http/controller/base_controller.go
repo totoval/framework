c9266d3805d3d148d39c1cb4f742331e47ecdd88
Toby Yan <me@tobyan.com>
2019-03-07 18:55:46 +0800

    add validation lang support
    Signed-off-by: Toby Yan <me@tobyan.com>

--- http/controller/base_controller.go
+++ http/controller/base_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/totoval/framework/resources/lang"
 	"gopkg.in/go-playground/validator.v9"
 	"net/http"
 )
@@ -16,9 +17,19 @@ type BaseController struct{}
 func (bc *BaseController) Validate(c *gin.Context, _validator interface{}) bool {
 	if err := c.ShouldBindBodyWith(_validator, binding.JSON); err != nil {
 
-		_ = err.(validator.ValidationErrors)
-		//@todo translate
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+		translator, exist := lang.Translator(lang.Locale(c))
+		if !exist {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			return false
+		}
+
+		_err, ok := err.(validator.ValidationErrors)
+		if !ok {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": err.Error()})
+			return false
+		}
+
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"error": _err.Translate(translator)})
 		return false
 	}
 
---
+++ resources/lang/translator.go
@@ -0,0 +1,22 @@
+package lang
+
+import (
+	"github.com/go-playground/locales"
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+)
+
+var UniversalTranslator *ut.UniversalTranslator
+
+func Initialize(){
+	_en := en.New()
+	UniversalTranslator = ut.New(_en, _en)
+}
+
+func AddLocale(translator locales.Translator){
+	UniversalTranslator.AddTranslator(translator, true)
+}
+
+func Translator(translator string) (ut.Translator, bool) {
+	return UniversalTranslator.GetTranslator(translator)
+}
