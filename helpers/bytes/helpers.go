6782275e4b67621fc5f638a8908cf8a62306d6d3
Toby Yan <me@tobyan.com>
2019-03-25 19:24:21 +0800

    add FromUint64 bytes helper
    Signed-off-by: Toby Yan <me@tobyan.com>

---
+++ helpers/bytes/helpers.go
@@ -0,0 +1,13 @@
+package bytes
+
+import (
+    "encoding/binary"
+)
+
+func FromUint64(input uint64) []byte {
+
+    theByte := make([]byte, 8)
+    binary.BigEndian.PutUint64(theByte, uint64(input))
+
+    return theByte
+}
