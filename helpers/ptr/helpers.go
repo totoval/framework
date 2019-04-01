6f183366a93031ba9c55e6285ebc4fc526e012d3
Toby Yan <me@tobyan.com>
2019-04-01 16:00:46 +0800

    gofmt
    Signed-off-by: Toby Yan <me@tobyan.com>

--- helpers/ptr/helpers.go
+++ helpers/ptr/helpers.go
@@ -1,54 +1,52 @@
 package ptr
 
 func String(value string) *string {
-    return &value
+	return &value
 }
 
 func MapStringInterface(value map[string]interface{}) *map[string]interface{} {
-    return &value
+	return &value
 }
 
 func Byte(value byte) *byte {
-    return &value
+	return &value
 }
 
 func Float64(value float64) *float64 {
-    return &value
+	return &value
 }
 func Float32(value float32) *float32 {
-    return &value
+	return &value
 }
 
 func Int(value int) *int {
-    return &value
+	return &value
 }
 func Int64(value int64) *int64 {
-    return &value
+	return &value
 }
 func Int32(value int32) *int32 {
-    return &value
+	return &value
 }
 func Int16(value int16) *int16 {
-    return &value
+	return &value
 }
 func Int8(value int8) *int8 {
-    return &value
+	return &value
 }
-
 
 func Uint(value uint) *uint {
-    return &value
+	return &value
 }
 func Uint64(value uint64) *uint64 {
-    return &value
+	return &value
 }
 func Uint32(value uint32) *uint32 {
-    return &value
+	return &value
 }
 func Uint16(value uint16) *uint16 {
-    return &value
+	return &value
 }
 func Uint8(value uint8) *uint8 {
-    return &value
+	return &value
 }
-
