04768fa1e44e673a43f9761df97b70333752b232
Toby Yan <me@tobyan.com>
2019-04-16 11:37:19 +0800

    fix migration table name issue
    Signed-off-by: Toby Yan <me@tobyan.com>

--- database/migration/migration.go
+++ database/migration/migration.go
@@ -1,37 +1,40 @@
 package migration
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/totoval/framework/config"
+    "fmt"
+
+    "github.com/jinzhu/gorm"
+
+    "github.com/totoval/framework/database"
+
+    "github.com/totoval/framework/config"
 )
 
 type Migration struct {
-	ID        uint   `gorm:"column:id;primary_key;auto_increment;"`
-	Migration string `gorm:"column:migration;type:varchar(255)"`
-	Batch     uint   `gorm:"column:batch;"`
+    ID        uint   `gorm:"column:id;primary_key;auto_increment;"`
+    Migration string `gorm:"column:migration;type:varchar(255)"`
+    Batch     uint   `gorm:"column:batch;"`
 }
 
 // 建立migration表
 func (m *Migration) up(db *gorm.DB) {
-	tx := db.Begin()
-	{
-		tx.CreateTable(&m)
-	}
-	tx.Commit()
+    tx := db.Begin()
+    {
+        tx.CreateTable(&m)
+    }
+    tx.Commit()
 }
 
 func (m *Migration) TableName() string {
-	//tableName, ok := config.Get("database.migrations").(string);
-	//if !ok {
-	//	panic("migrations table name parse error")
-	//}
-	tableName := config.GetString("database.migrations")
-	if tableName == "" {
-		panic("migrations table name parse error")
-	}
-	return tableName
+    tableName := config.GetString("database.migrations")
+    if tableName == "" {
+        panic("migrations table name parse error")
+    }
+
+    tableNameWithPrefix := fmt.Sprintf("%s%s", database.Prefix(), tableName)
+    return tableNameWithPrefix
 }
 
 func (m *Migration) Name() string {
-	return m.Migration
+    return m.Migration
 }
