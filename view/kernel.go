aecba7361141ee0bc68c620197293921f2fecfc8
Toby Yan <me@tobyan.com>
2019-04-17 10:41:14 +0800

    add html template support
    Signed-off-by: Toby Yan <me@tobyan.com>

---
+++ view/kernel.go
@@ -0,0 +1,23 @@
+package view
+
+import (
+    "html/template"
+
+    "github.com/gin-gonic/gin"
+
+    "github.com/totoval/framework/helpers/debug"
+)
+
+var templateList []*template.Template
+
+func Initialize(r *gin.Engine) {
+    for _, tmpl := range templateList {
+        r.SetHTMLTemplate(tmpl)
+    }
+}
+
+func AddView(name string, content string) {
+    debug.Dump(name)
+    tmpl := template.Must(template.New(name).Parse(content))
+    templateList = append(templateList, tmpl)
+}
