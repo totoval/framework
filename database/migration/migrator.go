00ed7df8eba210aed239269e827f5e09ac993f00
Toby Yan <me@tobyan.com>
2019-04-17 10:40:34 +0800

    format
    Signed-off-by: Toby Yan <me@tobyan.com>

--- database/migration/migrator.go
+++ database/migration/migrator.go
@@ -1,27 +1,27 @@
 package migration
 
 import (
-	"github.com/jinzhu/gorm"
+    "github.com/jinzhu/gorm"
 )
 
 type Migrator interface {
-	Up(db *gorm.DB) *gorm.DB
-	Down(db *gorm.DB) *gorm.DB
-	MigratorIdentifier
+    Up(db *gorm.DB) *gorm.DB
+    Down(db *gorm.DB) *gorm.DB
+    MigratorIdentifier
 }
 
 // contains all the migrators
 var migratorList []Migrator
 
 func AddMigrator(migrator Migrator) {
-	migratorList = append(migratorList, migrator)
+    migratorList = append(migratorList, migrator)
 }
 
 func newMigrator(name string) Migrator {
-	for _, migrator := range migratorList {
-		if name == migrator.Name(&migrator) {
-			return migrator
-		}
-	}
-	return nil
+    for _, migrator := range migratorList {
+        if name == migrator.Name(&migrator) {
+            return migrator
+        }
+    }
+    return nil
 }
