bdab6d3c2a685c9ead5f947f6450d2862cf2f915
Toby Yan <me@tobyan.com>
2019-05-14 15:18:31 +0800

    more beautiful debug message
    Signed-off-by: Toby Yan <me@tobyan.com>

--- helpers/debug/helpers.go
+++ helpers/debug/helpers.go
@@ -1,24 +1,18 @@
 package debug
 
 import (
-	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"os"
+
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/totoval/framework/cmd"
 )
 
 func Dump(v ...interface{}) {
-	fmt.Println("########### Totoval Dump ###########")
-	for _, value := range v {
-		spew.Dump(value)
-	}
-	fmt.Println("########### Totoval Dump ###########")
+	cmd.Println(cmd.CODE_WARNING, spew.Sdump(v...))
 }
 
 func DD(v ...interface{}) {
-	fmt.Println("########### Totoval DD ###########")
-	for _, value := range v {
-		spew.Dump(value)
-	}
-	fmt.Println("########### Totoval DD ###########")
+	cmd.Println(cmd.CODE_ERROR, spew.Sdump(v...))
 	os.Exit(1)
 }
