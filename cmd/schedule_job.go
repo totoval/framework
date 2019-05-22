7b3e2160e0304132cb9a99863c1dc47cb56af7a4
Toby Yan <me@tobyan.com>
2019-05-22 18:41:50 +0800

    handle schedule panic
    Signed-off-by: Toby Yan <me@tobyan.com>

--- cmd/schedule_job.go
+++ cmd/schedule_job.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "log"
+import (
+	"errors"
+
+	"github.com/totoval/framework/helpers/log"
+)
 
 type job struct {
 	scmd *scheduleCommand
@@ -11,7 +15,19 @@ func NewJob(scmd *scheduleCommand) *job {
 }
 
 func (j *job) Run() {
+	defer func(command *scheduleCommand) {
+		if err := recover(); err != nil {
+			var __err error
+			if _err, ok := err.(error); ok {
+				__err = _err
+			} else {
+				__err = errors.New(command.When() + "schedule panic") //@todo err.(string) may be down when `panic(123)`
+			}
+			_ = log.Error(__err)
+		}
+	}(j.scmd)
+
 	if err := j.scmd.handler()(j.scmd.args()); err != nil {
-		log.Println("......schedule run failed......", err)
+		_ = log.Error(err)
 	}
 }
