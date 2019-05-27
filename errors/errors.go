7a4e932a2a820d868efcd4a24b25783d771e04b2
Toby Yan <me@tobyan.com>
2019-05-27 17:34:00 +0800

    fix a bug
    Signed-off-by: Toby Yan <me@tobyan.com>

--- errors/errors.go
+++ errors/errors.go
@@ -20,7 +20,9 @@ func ErrPrintln(err error, startFrom int, fields logs.Field) {
 
 	traceErr = tracerr.CustomError(err, frameList[startFrom:len(frameList)-2])
 
-	fields["totoval_trace"] = tracerr.SprintSource(traceErr)
+	if fields != nil {
+		fields["totoval_trace"] = tracerr.SprintSource(traceErr)
+	}
 	logs.Println(logs.ERROR, err.Error(), fields)
 }
 
@@ -35,7 +37,9 @@ func ErrPrint(err error, startFrom int, fields logs.Field) string {
 	}
 
 	traceErr = tracerr.CustomError(err, frameList[startFrom:len(frameList)-2])
-
-	fields["totoval_trace"] = tracerr.SprintSource(traceErr)
+	
+	if fields != nil {
+		fields["totoval_trace"] = tracerr.SprintSource(traceErr)
+	}
 	return fmt.Sprint(err.Error(), fields)
 }
