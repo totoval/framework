6e91a2306f80afb0dced764a7152a93fe06aded1
Toby Yan <me@tobyan.com>
2019-05-30 17:44:13 +0800

    remove common cache
    Signed-off-by: Toby Yan <me@tobyan.com>

--- cache/driver/memory/memory.go
+++ cache/driver/memory/memory.go
@@ -7,14 +7,13 @@ import (
 	"github.com/jinzhu/copier"
 	c "github.com/patrickmn/go-cache"
 
-	"github.com/totoval/framework/cache/driver/common"
 	. "github.com/totoval/framework/cache/utils"
 	"github.com/totoval/framework/helpers/zone"
 )
 
 func NewMemory(prefix string, defaultExpirationMinute uint, cleanUpIntervalMinute uint) *memory {
 	return &memory{
-		memoryBasic: memoryBasic{
+		memoryBasic{
 			cache:  c.New(zone.Duration(defaultExpirationMinute)*zone.Minute, zone.Duration(cleanUpIntervalMinute)*zone.Minute),
 			prefix: prefix,
 		},
@@ -23,7 +22,6 @@ func NewMemory(prefix string, defaultExpirationMinute uint, cleanUpIntervalMinut
 
 type memory struct {
 	memoryBasic
-	common.Common
 }
 
 func (m *memory) Pget(key string, valuePtr proto.Message, defaultValuePtr ...proto.Message) error {
@@ -47,4 +45,42 @@ func (m *memory) Pget(key string, valuePtr proto.Message, defaultValuePtr ...pro
 		return err
 	}
 	return nil
+}
+
+// ------------------------------------------------------------------------------
+// the same
+// ------------------------------------------------------------------------------
+
+func (m *memory) Ppull(key string, valuePtr proto.Message, defaultValuePtr ...proto.Message) error {
+	k := NewKey(key, m.Prefix())
+
+	err := m.Pget(k.Raw(), valuePtr, defaultValuePtr...)
+	if err != nil {
+		return err
+	}
+
+	m.Forget(k.Raw())
+
+	return nil
+}
+func (m *memory) Pput(key string, valuePtr proto.Message, future zone.Time) bool {
+	valueBytes, err := proto.Marshal(valuePtr)
+	if err != nil {
+		return false
+	}
+	return m.Put(key, valueBytes, future)
+}
+func (m *memory) Padd(key string, valuePtr proto.Message, future zone.Time) bool {
+	valueBytes, err := proto.Marshal(valuePtr)
+	if err != nil {
+		return false
+	}
+	return m.Add(key, valueBytes, future)
+}
+func (m *memory) Pforever(key string, valuePtr proto.Message) bool {
+	valueBytes, err := proto.Marshal(valuePtr)
+	if err != nil {
+		return false
+	}
+	return m.Forever(key, valueBytes)
 }
--- cache/driver/redis/redis.go
+++ cache/driver/redis/redis.go
@@ -7,8 +7,8 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/jinzhu/copier"
 
-	"github.com/totoval/framework/cache/driver/common"
 	. "github.com/totoval/framework/cache/utils"
+	"github.com/totoval/framework/helpers/zone"
 )
 
 func NewRedis(host string, port string, password string, dbIndex int, prefix string) *redis {
@@ -23,7 +23,7 @@ func NewRedis(host string, port string, password string, dbIndex int, prefix str
 	//// Output: PONG <nil>
 
 	return &redis{
-		redisBasic: redisBasic{
+		redisBasic{
 			cache:  client,
 			prefix: prefix,
 		},
@@ -32,7 +32,6 @@ func NewRedis(host string, port string, password string, dbIndex int, prefix str
 
 type redis struct {
 	redisBasic
-	common.Common
 }
 
 func (re *redis) Pget(key string, valuePtr proto.Message, defaultValuePtr ...proto.Message) error {
@@ -57,4 +56,42 @@ func (re *redis) Pget(key string, valuePtr proto.Message, defaultValuePtr ...pro
 		return err
 	}
 	return nil
+}
+
+// ------------------------------------------------------------------------------
+// the same
+// ------------------------------------------------------------------------------
+
+func (re *redis) Ppull(key string, valuePtr proto.Message, defaultValuePtr ...proto.Message) error {
+	k := NewKey(key, re.Prefix())
+
+	err := re.Pget(k.Raw(), valuePtr, defaultValuePtr...)
+	if err != nil {
+		return err
+	}
+
+	re.Forget(k.Raw())
+
+	return nil
+}
+func (re *redis) Pput(key string, valuePtr proto.Message, future zone.Time) bool {
+	valueBytes, err := proto.Marshal(valuePtr)
+	if err != nil {
+		return false
+	}
+	return re.Put(key, valueBytes, future)
+}
+func (re *redis) Padd(key string, valuePtr proto.Message, future zone.Time) bool {
+	valueBytes, err := proto.Marshal(valuePtr)
+	if err != nil {
+		return false
+	}
+	return re.Add(key, valueBytes, future)
+}
+func (re *redis) Pforever(key string, valuePtr proto.Message) bool {
+	valueBytes, err := proto.Marshal(valuePtr)
+	if err != nil {
+		return false
+	}
+	return re.Forever(key, valueBytes)
 }
