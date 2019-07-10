e5a0c7c5b5fc0fecdb4994e049669b20029915e8
Toby Yan <me@tobyan.com>
2019-07-10 18:01:17 +0800

    fix a bug when route group prefix is empty
    Signed-off-by: Toby Yan <me@tobyan.com>

--- route/group.go
+++ route/group.go
@@ -40,54 +40,77 @@ type group struct {
 }
 
 func (g *group) Handle(httpMethod, relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute(httpMethod, g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.Handle(httpMethod, relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) Any(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
-	return newRoute("Any", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.Any(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
+	relativePath = g.clearPath(relativePath)
+	return newRoute("Any", g, g.clearPath(relativePath), func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.Any(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) GET(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("GET", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.GET(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) POST(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("POST", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.POST(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) DELETE(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("DELETE", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.DELETE(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) PATCH(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("PATCH", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.PATCH(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) PUT(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("PUT", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.PUT(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) OPTIONS(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("OPTIONS", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.OPTIONS(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
 func (g *group) HEAD(relativePath string, handlers ...request.HandlerFunc) policy.RoutePolicier {
+	relativePath = g.clearPath(relativePath)
 	return newRoute("HEAD", g, relativePath, func(innerHandlers ...request.HandlerFunc) { g.RouterGroup.HEAD(relativePath, request.ConvertHandlers(innerHandlers)...) }, handlers...)
 }
 
-//func (g *group) StaticFile(relativePath, filepath string) gin.IRoutes {
-//	return g.RouterGroup.StaticFile(relativePath, filepath)
-//}
-//
-//func (g *group) Static(relativePath, root string) gin.IRoutes {
-//	return g.RouterGroup.Static(relativePath, root)
-//}
-//
-//func (g *group) StaticFS(relativePath string, fs http.FileSystem) gin.IRoutes {
-//	return g.RouterGroup.StaticFS(relativePath, fs)
-//}
+func (g *group) StaticFile(relativePath, filepath string) gin.IRoutes {
+	relativePath = g.clearPath(relativePath)
+	return g.RouterGroup.StaticFile(relativePath, filepath)
+}
+
+func (g *group) Static(relativePath, root string) gin.IRoutes {
+	relativePath = g.clearPath(relativePath)
+	return g.RouterGroup.Static(relativePath, root)
+}
+
+func (g *group) StaticFS(relativePath string, fs http.FileSystem) gin.IRoutes {
+	relativePath = g.clearPath(relativePath)
+	return g.RouterGroup.StaticFS(relativePath, fs)
+}
 
 func (g *group) AddGroup(relativePath string, routeGrouper RouteGrouper, handlers ...request.HandlerFunc) {
 	ginGroup := g.RouterGroup.Group(relativePath, request.ConvertHandlers(handlers)...)
 	routeGrouper.Group(&group{RouterGroup: ginGroup})
+}
+
+func (g *group) clearPath(relativePath string) string {
+	basePath := g.RouterGroup.BasePath()
+	if basePath[len(basePath)-1:] == "/" && relativePath[:1] == "/" {
+		return relativePath[1:]
+	}
+	if basePath[len(basePath)-1:] != "/" && relativePath[:1] != "/" {
+		return "/" + relativePath
+	}
+	return relativePath
 }
