332f327a431169aaf44d24fd8c12c7da88405086
Toby Yan <me@tobyan.com>
2019-07-25 15:59:49 +0800

    Add some validate functions
    * ValidateJSON(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateXML(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateYAML(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateMsgPack(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateProtoBuf(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    
    * ValidateJSONMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateXMLMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateYAMLMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateMsgPackMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateProtoBufMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    
    * ValidateUri(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateQuery(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateForm(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateFormPost(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    * ValidateFormMultipart(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool)
    
    Signed-off-by: Toby Yan <me@tobyan.com>

--- validator/validation.go
+++ validator/validation.go
@@ -3,6 +3,7 @@ package validator
 import (
 	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 
 	"gopkg.in/go-playground/validator.v9"
@@ -15,8 +16,109 @@ import (
 type Validation struct {
 }
 
-func (v *Validation) Validate(c Context, _validator interface{}, onlyFirstError bool) (isAbort bool) {
-	if err := c.ShouldBindBodyWith(_validator, binding.JSON); err != nil {
+//@todo Deprecated, for compatible with v0.10.0
+func (v *Validation) Validate(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return v.ValidateJSON(c, requestDataPtr, onlyFirstError)
+}
+
+// not consume request body
+func (v *Validation) ValidateJSONMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindBodyWith(rdp, binding.JSON)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateXMLMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindBodyWith(rdp, binding.XML)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateYAMLMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindBodyWith(rdp, binding.YAML)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateMsgPackMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindBodyWith(rdp, binding.MsgPack)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateProtoBufMulti(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindBodyWith(rdp, binding.ProtoBuf)
+	}, c, requestDataPtr, onlyFirstError)
+}
+
+// consume request body
+func (v *Validation) ValidateJSON(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	if hasBodyBytesKey(c) {
+		return v.ValidateJSONMulti(c, requestDataPtr, onlyFirstError)
+	}
+
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindWith(rdp, binding.JSON)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateXML(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	if hasBodyBytesKey(c) {
+		return v.ValidateXMLMulti(c, requestDataPtr, onlyFirstError)
+	}
+
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindWith(rdp, binding.XML)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateYAML(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	if hasBodyBytesKey(c) {
+		return v.ValidateYAMLMulti(c, requestDataPtr, onlyFirstError)
+	}
+
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindWith(rdp, binding.YAML)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateMsgPack(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	if hasBodyBytesKey(c) {
+		return v.ValidateMsgPackMulti(c, requestDataPtr, onlyFirstError)
+	}
+
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindWith(rdp, binding.MsgPack)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateProtoBuf(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	if hasBodyBytesKey(c) {
+		return v.ValidateProtoBufMulti(c, requestDataPtr, onlyFirstError)
+	}
+
+	return validate(func(rdp interface{}) error {
+		return c.ShouldBindWith(rdp, binding.ProtoBuf)
+	}, c, requestDataPtr, onlyFirstError)
+}
+func hasBodyBytesKey(c Context) bool {
+	if _, ok := c.Get(gin.BodyBytesKey); ok {
+		return true
+	}
+	return false
+}
+
+func (v *Validation) ValidateUri(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(c.ShouldBindUri, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateQuery(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(c.ShouldBindQuery, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateForm(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(c.ShouldBind, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateFormPost(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(c.ShouldBind, c, requestDataPtr, onlyFirstError)
+}
+func (v *Validation) ValidateFormMultipart(c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	return validate(c.ShouldBind, c, requestDataPtr, onlyFirstError)
+}
+
+func validate(shouldBindFunc func(rdp interface{}) error, c Context, requestDataPtr interface{}, onlyFirstError bool) (isAbort bool) {
+	if err := shouldBindFunc(requestDataPtr); err != nil {
 
 		_err, ok := err.(validator.ValidationErrors)
 		if !ok {
