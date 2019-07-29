95642ddf728ee104c17517b3fc5c76ac7933734b
Toby Yan <me@tobyan.com>
2019-07-29 18:12:20 +0800

    add basic_auth, basic_auth_for_realm middleware
    Signed-off-by: Toby Yan <me@tobyan.com>

---
+++ http/middleware/basic_auth.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/totoval/framework/request"
+)
+
+func BasicAuth(accounts map[string]string) request.HandlerFunc {
+	return func(c *request.Context) {
+		gin.BasicAuth(accounts)(c.Context)
+	}
+}
---
+++ http/middleware/basic_auth_for_realm.go
@@ -0,0 +1,13 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/totoval/framework/request"
+)
+
+func BasicAuthWithRealm(accounts map[string]string, realm string) request.HandlerFunc {
+	return func(c *request.Context) {
+		gin.BasicAuthForRealm(accounts, realm)(c.Context)
+	}
+}
